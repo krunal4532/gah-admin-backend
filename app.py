11fa20c9b6da8acd4e85f82e4837f12d1ef90758
Krunal <krunal505@gmail.com>
2025-07-08 16:55:08 +0530

    Update DB connection to NeonDB

--- app.py
+++ app.py
@@ -10,12 +10,16 @@ app.secret_key = os.environ.get("SECRET_KEY", "defaultsecretkey")
 
 # --- DB Connection ---
 def get_db_connection():
+    import psycopg2
+    import psycopg2.extras
+    import os
+
     return psycopg2.connect(
         host=os.environ.get("DB_HOST"),
         database=os.environ.get("DB_NAME"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        sslmode="require",
+        port=os.environ.get("DB_PORT", 5432),
         cursor_factory=psycopg2.extras.RealDictCursor
     )
 
